acf3dad9a657c4b5395848ecbec84bf96eeadc22
Zararthustra <70105093+Zararthustra@users.noreply.github.com>
2024-01-16 11:54:04 +0100

    feat(Home): add pages links

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,11 @@
-import { IconLogo } from "@assets/index";
+import {
+  IconFood,
+  IconFormula,
+  IconIntolerance,
+  IconKhayat,
+  IconLogo,
+} from "@assets/index";
+import { Button } from "@components/index";
 import { useMediaQuery } from "react-responsive";
 
 export const Home = () => {
@@ -6,15 +13,34 @@ export const Home = () => {
 
   return (
     <>
-      <main className="flex h-[500px] flex-col items-center justify-center gap-10 px-2">
+      <main className="flex h-[80vh] flex-col items-center justify-evenly gap-5 px-2">
         <IconLogo
-          className="dark:text-slate-100"
+          className="shrink-0 dark:text-slate-100"
           width={isMobile ? 200 : 300}
           height={isMobile ? 200 : 300}
         />
         <h1 className="text-center dark:text-slate-100">
           L'assistant des nutritionnistes
         </h1>
+
+        <div className="mt-5 flex flex-col gap-2">
+          <Button primary className="justify-between">
+            <IconFormula />
+            Formules et indices
+          </Button>
+          <Button primary className="justify-between">
+            <IconIntolerance />
+            Liste des intolérances
+          </Button>
+          <Button primary className="justify-between">
+            <IconFood />
+            Informations sur les aliments
+          </Button>
+          <Button primary className="justify-between">
+            <IconKhayat />
+            Fiches du Pr Khayat
+          </Button>
+        </div>
       </main>
     </>
   );
