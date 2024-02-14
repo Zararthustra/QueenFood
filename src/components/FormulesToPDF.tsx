08418bf8df83aab82ff14cf5d6b4693f09ddad1f
Zararthustra <70105093+Zararthustra@users.noreply.github.com>
2024-02-14 18:08:05 +0100

    fix: split pages

--- src/components/FormulesToPDF.tsx
+++ src/components/FormulesToPDF.tsx
@@ -9,9 +9,14 @@ import {
   View
 } from '@react-pdf/renderer';
 
-import { background } from '@assets/index';
 import { IconError, IconPDF } from '@assets/index';
-import { Button, IMATable, IMCTable, IMGTable } from '@components/index';
+import {
+  Button,
+  HeaderPDF,
+  IMATable,
+  IMCTable,
+  IMGTable
+} from '@components/index';
 import { IMCGAChartImage, MBChartImage, MNChartImage } from '@data/index';
 import { IFormulesForm } from '@interfaces/index';
 
@@ -161,90 +166,10 @@ export const FormulesToPDF = ({ form, patient, data }: IFormulesToPDFProps) => {
       language="fr">
       <Page size="A4" orientation="portrait" style={styles.page}>
         {/* Header */}
-        <Image
-          src={background}
-          style={{
-            width: '100%',
-            height: '360px',
-            position: 'absolute',
-            objectFit: 'cover'
-          }}
-        />
-        <View
-          style={{
-            fontSize: '32px',
-            fontWeight: 800,
-            marginTop: '40px',
-            marginHorizontal: '45px',
-            alignSelf: 'flex-end',
-            display: 'flex',
-            alignItems: 'center',
-            color: '#2f424b'
-          }}>
-          <Text>Rapport</Text>
-          <Text>Nutritionnel</Text>
-        </View>
+        <HeaderPDF styles={styles} form={form} patient={patient} />
+      </Page>
 
-        {/* Labels */}
-        <View
-          style={{
-            width: '150px',
-            marginBottom: '40px',
-            marginHorizontal: '74px',
-            alignSelf: 'flex-end'
-          }}
-          {...{ bookmark: 'Informations' }}>
-          <View
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              gap: '10px',
-              marginVertical: '40px'
-            }}>
-            <View
-              style={{
-                width: '100px',
-                height: '1px',
-                backgroundColor: '#a5c8d4'
-              }}
-            />
-            <View
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'center',
-                gap: '5px',
-                fontWeight: 600
-              }}>
-              <Text style={styles.textInfo}>{patient.firstname}</Text>
-              <Text style={styles.textInfo}>{patient.lastname}</Text>
-            </View>
-            <View
-              style={{
-                width: '100px',
-                height: '1px',
-                backgroundColor: '#a5c8d4'
-              }}
-            />
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.label}>Âge :</Text>
-            <Text style={styles.textInfo}>{form.age} ans</Text>
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.label}>Poids :</Text>
-            <Text style={styles.textInfo}>{form.weight} kg</Text>
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.label}>Taille :</Text>
-            <Text style={styles.textInfo}>{form.height} cm</Text>
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.label}>Tour de taille :</Text>
-            <Text style={styles.textInfo}>{form.hip} cm</Text>
-          </View>
-        </View>
-
+      <Page size="A4" orientation="portrait" style={styles.page}>
         {/* Indices */}
         <View {...{ bookmark: 'Indices' }}>
           <View
